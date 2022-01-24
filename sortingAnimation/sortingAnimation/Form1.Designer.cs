40b601f7c25fd969153afebebb6af520391b6726
Quintton <quintontiller@gmail.com>
2022-01-23 16:30:55 -0800

    full upload

---
+++ sortingAnimation/sortingAnimation/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace sortingAnimation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate Array";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(64, 241);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 65);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(136, 241);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 65);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "SelectionSort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Generate Assending Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(335, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Generate Desending Data";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(166, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "InsertionSort";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(64, 201);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(40, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(126, 201);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(42, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(196, 201);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(44, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(268, 201);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(46, 20);
+            this.textBox6.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(335, 201);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(46, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(268, 147);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(84, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "ShellSort";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(388, 147);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "BubbleSort";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "SortingAnimation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ sortingAnimation/sortingAnimation/Form1.cs
@@ -0,0 +1,399 @@
+﻿// Name: Quinton Tiller
+//Disciption: This programe will allow the user to generate an array and then select a sorting 
+// algorithm of their choice to watch step by step how it will sort the array.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortingAnimation
+{
+
+    public partial class Form1 : Form
+    {
+        int[] mainArray = new int[5];
+        List<TextBox> myTextboxList = new List<TextBox>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+
+        }
+
+        //Generate Array Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] array = GenerateArray();
+            mainArray = array;
+            string results = string.Join(".", array);
+
+            textBox1.Text = results;
+
+            textBox3.Text = results[0].ToString();
+            textBox4.Text = results[2].ToString();
+            textBox5.Text = results[4].ToString();
+            textBox6.Text = results[6].ToString();
+            textBox7.Text = results[8].ToString();
+
+        }
+
+        public int[] GenerateArray()
+        {
+            int Min = 0;
+            int Max = 9;
+            int[] array = new int[5];
+
+            Random randNum = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randNum.Next(Min, Max);
+            }
+            return array;
+        }
+
+        //Selection Sort BUtton
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myTextboxList.Clear();
+            myTextboxList.Add(textBox3);
+            myTextboxList.Add(textBox4);
+            myTextboxList.Add(textBox5);
+            myTextboxList.Add(textBox6);
+            myTextboxList.Add(textBox7);
+            timer1.Enabled = true;
+            pictureBox1.Top = textBox3.Bottom;
+            pictureBox1.Left = textBox3.Left;
+            pictureBox2.Top = textBox4.Bottom;
+            pictureBox2.Left = textBox4.Left;
+        }
+
+        //Assening button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] array = GenerateArray();
+            Array.Sort(array);
+
+            string results = string.Join(".", array);
+
+            textBox1.Text = results;
+
+            textBox3.Text = results[0].ToString();
+            textBox4.Text = results[2].ToString();
+            textBox5.Text = results[4].ToString();
+            textBox6.Text = results[6].ToString();
+            textBox7.Text = results[8].ToString();
+        }
+        //Desending Button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] array = GenerateArray();
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            string results = string.Join(".", array);
+
+            textBox1.Text = results;
+
+            textBox3.Text = results[0].ToString();
+            textBox4.Text = results[2].ToString();
+            textBox5.Text = results[4].ToString();
+            textBox6.Text = results[6].ToString();
+            textBox7.Text = results[8].ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            // One by one move boundary of unsorted subarray
+
+            for (int i = 0; i < myTextboxList.Count - 1; i++)
+            {
+
+                // Find the minimum element in unsorted array 
+                int min_idx = i;
+
+
+
+                highlightSmallest(i);
+
+
+                for (int j = i + 1; j < myTextboxList.Count; j++)
+
+
+                    if (int.Parse(myTextboxList[j].Text) < int.Parse(myTextboxList[min_idx].Text))
+                    {
+                        min_idx = j;
+
+                    }
+                highlightswap(min_idx);
+
+
+                // Swap the found minimum element with the first 
+                // element 
+                int temp = int.Parse(myTextboxList[min_idx].Text);
+                myTextboxList[min_idx].Text = myTextboxList[i].Text;
+                myTextboxList[i].Text = temp.ToString();
+
+
+
+            }
+            timer1.Enabled = false;
+        }
+        //InsertionSort Button
+        private void button5_Click(object sender, EventArgs e)
+        {
+            myTextboxList.Clear();
+            myTextboxList.Add(textBox3);
+            myTextboxList.Add(textBox4);
+            myTextboxList.Add(textBox5);
+            myTextboxList.Add(textBox6);
+            myTextboxList.Add(textBox7);
+            timer2.Enabled = true;
+            pictureBox1.Top = textBox3.Bottom;
+            pictureBox1.Left = textBox3.Left;
+            pictureBox2.Top = textBox4.Bottom;
+            pictureBox2.Left = textBox4.Left;
+        }
+        public void highlightSmallest(int i)
+        {
+
+            if (i == 0)
+            {
+                pictureBox1.Top = textBox3.Bottom;
+            }
+            if (i == 1)
+            {
+                pictureBox1.Top = textBox4.Bottom;
+                pictureBox1.Left = textBox4.Left;
+            }
+            if (i == 2)
+            {
+                pictureBox1.Top = textBox5.Bottom;
+                pictureBox1.Left = textBox5.Left;
+            }
+            if (i == 3)
+            {
+                pictureBox1.Top = textBox6.Bottom;
+                pictureBox1.Left = textBox6.Left;
+            }
+            if (i == 4)
+            {
+                pictureBox1.Top = textBox7.Bottom;
+                pictureBox1.Left = textBox7.Left;
+            }
+            int count = 0;
+            if (timer1.Enabled == true) {
+                timer1.Enabled = false;
+                count = 1;
+            }
+            else if (timer2.Enabled == true) {
+                timer2.Enabled = false;
+                count = 2;
+            }
+            else if (timer3.Enabled == true) {
+                timer3.Enabled = false;
+                count = 3;
+            }
+            else if (timer4.Enabled == true) {
+
+                timer4.Enabled = false;
+                count = 4;
+            }
+            DialogResult res = MessageBox.Show("Step", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (res == DialogResult.OK)
+            {
+                if (count == 1)
+                    timer1.Enabled = true;
+                else if (count == 2)
+                    timer2.Enabled = true;
+                else if (count == 3)
+                    timer3.Enabled = true;
+                else if (count == 4)
+                    timer4.Enabled = true;
+                
+            }
+            if (res == DialogResult.Cancel)
+            {
+                Environment.Exit(0);
+
+            }
+
+
+        }
+        public void highlightswap(int j)
+        {
+
+            if (j == 1)
+            {
+                pictureBox2.Top = textBox4.Bottom;
+                pictureBox2.Left = textBox4.Left;
+            }
+            if (j == 2)
+            {
+                pictureBox2.Top = textBox5.Bottom;
+                pictureBox2.Left = textBox5.Left;
+            }
+            if (j == 3)
+            {
+                pictureBox2.Top = textBox6.Bottom;
+                pictureBox2.Left = textBox6.Left;
+            }
+            if (j == 4)
+            {
+                pictureBox2.Top = textBox7.Bottom;
+                pictureBox2.Left = textBox7.Left;
+            }
+            int count = 0;
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+                count = 1;
+            }
+            else if (timer2.Enabled == true)
+            {
+                timer2.Enabled = false;
+                count = 2;
+            }
+            else if (timer3.Enabled == true)
+            {
+                timer3.Enabled = false;
+                count = 3;
+            }
+            else if (timer4.Enabled == true)
+            {
+                timer4.Enabled = false;
+                count = 4;
+            }
+
+            DialogResult res = MessageBox.Show("Step", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (res == DialogResult.OK)
+            {
+                if (count == 1)
+                    timer1.Enabled = true;
+                else if (count == 2)
+                    timer2.Enabled = true;
+                else if (count == 3)
+                    timer3.Enabled = true;
+                else if (count == 4)
+                    timer4.Enabled = true;
+            }
+            if (res == DialogResult.Cancel)
+            {
+                Environment.Exit(0);
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < myTextboxList.Count - 1; i++)
+            {
+                highlightSmallest(i);
+                for (int j = i + 1; j > 0; j--)
+                {
+                    highlightswap(j);
+                    if (int.Parse(myTextboxList[j - 1].Text) > int.Parse(myTextboxList[j].Text))
+                    {
+                        int temp = int.Parse(myTextboxList[j - 1].Text);
+                        myTextboxList[j - 1].Text = myTextboxList[j].Text;
+                        myTextboxList[j].Text = temp.ToString();
+                    }
+                }
+            }
+            timer2.Enabled = false;
+        }
+        //ShellSort Button
+        private void button6_Click(object sender, EventArgs e)
+        {
+            myTextboxList.Clear();
+            myTextboxList.Add(textBox3);
+            myTextboxList.Add(textBox4);
+            myTextboxList.Add(textBox5);
+            myTextboxList.Add(textBox6);
+            myTextboxList.Add(textBox7);
+            timer3.Enabled = true;
+            pictureBox1.Top = textBox3.Bottom;
+            pictureBox1.Left = textBox3.Left;
+            pictureBox2.Top = textBox4.Bottom;
+            pictureBox2.Left = textBox4.Left;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int n = myTextboxList.Count;
+            for (int gap = n / 2; gap > 0; gap /= 2)
+            {
+                // Do a gapped insertion sort for this gap size. 
+                // The first gap elements a[0..gap-1] are already in gapped order 
+                // keep adding one more element until the entire array is 
+                // gap sorted  
+                for (int i = gap; i < n; i += 1)
+                {
+                    highlightSmallest(i);
+                    // add a[i] to the elements that have been gap sorted 
+                    // save a[i] in temp and make a hole at position i 
+                    int temp = int.Parse(myTextboxList[i].Text);
+
+                    // shift earlier gap-sorted elements up until the correct  
+                    // location for a[i] is found 
+                    int j;
+                    for (j = i; j >= gap && int.Parse(myTextboxList[j - gap].Text) > temp; j -= gap)
+                        myTextboxList[j].Text = myTextboxList[j - gap].Text;
+                    highlightswap(j);
+
+                    //  put temp (the original a[i]) in its correct location 
+                    myTextboxList[j].Text = temp.ToString();
+                }
+            }
+            timer3.Enabled = false;
+        }
+        //BubbleSort Button
+        private void button7_Click(object sender, EventArgs e)
+        {
+            myTextboxList.Clear();
+            myTextboxList.Add(textBox3);
+            myTextboxList.Add(textBox4);
+            myTextboxList.Add(textBox5);
+            myTextboxList.Add(textBox6);
+            myTextboxList.Add(textBox7);
+            timer4.Enabled = true;
+            pictureBox1.Top = textBox3.Bottom;
+            pictureBox1.Left = textBox3.Left;
+            pictureBox2.Top = textBox4.Bottom;
+            pictureBox2.Left = textBox4.Left;
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            int n = myTextboxList.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                highlightSmallest(i);
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    highlightswap(j);
+                    if (int.Parse(myTextboxList[j].Text) > int.Parse(myTextboxList[j + 1].Text))
+                    {
+                        // swap temp and arr[i] 
+                        int temp = int.Parse(myTextboxList[j].Text);
+                        myTextboxList[j].Text = myTextboxList[j + 1].Text;
+                        myTextboxList[j + 1].Text = temp.ToString();
+                    }
+                }
+            }
+            timer4.Enabled = false;
+        }
+    }
+}
+
